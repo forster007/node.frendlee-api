b205e0ac2b2597899642bfd4621b51fd02003921
Thomas Förster <thomasforster@MacBook-Pro-de-Thomas.local>
2020-02-12 23:53:29 -0300

    mid dev

--- src/database/seeders/20191201100001-seed-users.js
+++ src/database/seeders/20191201100001-seed-users.js
@@ -5,7 +5,7 @@ module.exports = {
       [
         {
           account_type: 'administrator',
-          email: 'thomas_forster@hotmail.com.br',
+          email: 'forster000@hotmail.com.br',
           password_hash:
             '$2y$08$a9/fbQ5EUt5xHjOTIpN51Ozrq5gNATGMf1koRl2rPf6st00uCzudm',
           status: 'enabled',
@@ -14,7 +14,7 @@ module.exports = {
         },
         {
           account_type: 'customer',
-          email: 'imperator.03@hotmail.com',
+          email: 'imperator000@hotmail.com',
           password_hash:
             '$2y$08$Ab5XuM1hi6xmQRnH3iIJPOF.hTtWuXUgOIuVrAk2josppnLC4T6A6',
           status: 'enabled',
@@ -23,7 +23,7 @@ module.exports = {
         },
         {
           account_type: 'provider',
-          email: 'forster007@gmail.com',
+          email: 'forster000@gmail.com',
           password_hash:
             '$2y$08$Ab5XuM1hi6xmQRnH3iIJPOF.hTtWuXUgOIuVrAk2josppnLC4T6A6',
           status: 'enabled',
@@ -32,7 +32,7 @@ module.exports = {
         },
         {
           account_type: 'provider',
-          email: 'andre@gmail.com',
+          email: 'scherer000@gmail.com',
           password_hash:
             '$2y$08$Ab5XuM1hi6xmQRnH3iIJPOF.hTtWuXUgOIuVrAk2josppnLC4T6A6',
           status: 'enabled',
