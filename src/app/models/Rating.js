6a396fe080549f1d896304cce9bceaff5cb04323
Thomas Förster <thomasforster@MacBook-Pro-de-Thomas.local>
2020-05-31 00:48:07 -0300

    mid dev

--- src/app/models/Rating.js
+++ src/app/models/Rating.js
@@ -5,8 +5,20 @@ class Rating extends Model {
     super.init(
       {
         appointment_id: Sequelize.INTEGER,
+        customer_comment: Sequelize.STRING,
+        customer_compliment: {
+          defaultValue: 'opened',
+          type: Sequelize.ENUM,
+          values: ['nice', 'organized', 'professional', 'informed', 'effective', 'mannerly'],
+        },
         customer_id: Sequelize.INTEGER,
         customer_rating: Sequelize.INTEGER,
+        provider_comment: Sequelize.STRING,
+        provider_compliment: {
+          defaultValue: 'opened',
+          type: Sequelize.ENUM,
+          values: ['nice', 'organized', 'professional', 'informed', 'effective', 'mannerly'],
+        },
         provider_id: Sequelize.INTEGER,
         provider_rating: Sequelize.INTEGER,
       },
