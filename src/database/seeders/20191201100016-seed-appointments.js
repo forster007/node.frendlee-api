c88317a72be3987145a6b36b9bda1c7df822331e
Thomas Förster <thomasforster@MacBook-Pro-de-Thomas.local>
2020-06-08 00:33:11 -0300

    mid dev

--- src/database/seeders/20191201100016-seed-appointments.js
+++ src/database/seeders/20191201100016-seed-appointments.js
@@ -15,7 +15,7 @@ module.exports = {
             .format(),
           observation:
             'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-          status: 'opened',
+          status: 'confirmed',
           value: 150.0,
           address: 'Rua São Nicolau, 480 - Estância Velha, Canoas - RS, Brasil',
           customer_id: 1,
