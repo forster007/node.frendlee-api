7d50cffc6896beb0ef47297c9a350a23849422a9
Thomas Forster <forster007@gmail.com>
2020-11-04 00:14:53 -0300

    mid dev

--- src/database/migrations/20191201000004-create-customers.js
+++ src/database/migrations/20191201000004-create-customers.js
@@ -26,6 +26,9 @@ module.exports = {
         type: Sequelize.ENUM,
         values: ['low', 'normal', 'high'],
       },
+      description: {
+        type: Sequelize.STRING,
+      },
       gender: {
         allowNull: false,
         type: Sequelize.ENUM,
