4772db21cf4c9a9b90393adf9413ea078576fdee
Forster Thomas <thomas@piercetechnology.co.uk>
2020-12-21 14:25:27 -0300

    changed for tests

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ app.listen(process.env.PORT, () => {
   const DATE = moment().format('DD/MM/YYYY HH:mm:ss');
   const { PORT } = process.env;
 
-  console.log(`Started server at ${DATE} on port ${PORT}`);
+  console.log(`Frendlee server started at ${DATE} on port ${PORT}`);
 });
